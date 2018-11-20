9142adf5f5c2e289dd54c4d9a869abe96d7228cb
Richard Lewis <richalewis@gmail.com>
2018-11-20 12:04:09 -0800

    Added test from FxNdf

--- src/test/java/com/opengamma/strata/calc/FxForwardTest.java
+++ src/test/java/com/opengamma/strata/calc/FxForwardTest.java
@@ -1,5 +1,6 @@
 package com.opengamma.strata.calc;
 
+import static com.opengamma.strata.basics.currency.Currency.USD;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
@@ -10,7 +11,6 @@ import java.time.LocalTime;
 import java.time.Period;
 
 import org.joda.beans.ser.JodaBeanSer;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,7 +24,11 @@ import com.opengamma.strata.basics.currency.CurrencyPair;
 import com.opengamma.strata.basics.currency.FxRate;
 import com.opengamma.strata.basics.date.BusinessDayAdjustment;
 import com.opengamma.strata.basics.date.BusinessDayConventions;
+import com.opengamma.strata.basics.date.DaysAdjustment;
 import com.opengamma.strata.basics.date.HolidayCalendarId;
+import com.opengamma.strata.basics.date.HolidayCalendarIds;
+import com.opengamma.strata.basics.index.FxIndex;
+import com.opengamma.strata.basics.index.ImmutableFxIndex;
 import com.opengamma.strata.calc.marketdata.BuiltMarketData;
 import com.opengamma.strata.calc.runner.CalculationFunctions;
 import com.opengamma.strata.market.curve.RatesCurveGroupDefinition;
@@ -36,6 +40,8 @@ import com.opengamma.strata.measure.rate.RatesMarketDataLookup;
 import com.opengamma.strata.product.AttributeType;
 import com.opengamma.strata.product.Trade;
 import com.opengamma.strata.product.TradeInfo;
+import com.opengamma.strata.product.fx.FxNdf;
+import com.opengamma.strata.product.fx.FxNdfTrade;
 import com.opengamma.strata.product.fx.FxSingle;
 import com.opengamma.strata.product.fx.FxSingleTrade;
 
@@ -51,21 +57,27 @@ public class FxForwardTest {
 
 	private static final LocalDate VAL_DATE = LocalDate.of(2018, 7, 30);
 
-	private static final CurrencyPair GBP_USD = CurrencyPair.of(Currency.GBP, Currency.USD);
+	private static final CurrencyPair USD_KRW = CurrencyPair.of(Currency.USD, Currency.KRW);
 	private static final CurrencyPair USD_CAD = CurrencyPair.of(Currency.USD, Currency.CAD);
 	private static final FxSingleTrade USD_CAD_FWD_6M = createFxForward(USD_CAD, 100000, 0.76, VAL_DATE,
 			Period.ofMonths(6));
+	private static final FxNdfTrade USD_KRW_NDF_6M = createFxNdf(USD_KRW, 100000, 1129.90, VAL_DATE,
+			Period.ofMonths(6));
 
-	private final static FxSingleTrade createFxForward(CurrencyPair pair, double amount, double rate,
+	private static final LocalDate calcPaymentDate(CurrencyPair pair, LocalDate valueDate, Period tenor) {
+
+		return HolidayCalendarId.defaultByCurrency(pair.getBase()).resolve(REF_DATA).nextOrSame(valueDate.plus(tenor));
+
+	}
+
+	private static final FxSingleTrade createFxForward(CurrencyPair pair, double amount, double rate,
 			LocalDate valueDate, Period tenor) {
 
-		HolidayCalendarId paymentCalId = HolidayCalendarId.defaultByCurrency(pair.getBase());
-
-		LocalDate paymentDate = HolidayCalendarId.defaultByCurrency(pair.getBase()).resolve(REF_DATA)
-				.nextOrSame(valueDate.plus(tenor));
+		LocalDate paymentDate = calcPaymentDate(pair, valueDate, tenor);
 
 		FxSingle fx = FxSingle.of(CurrencyAmount.of(pair.getCounter(), amount), FxRate.of(pair, rate), paymentDate,
-				BusinessDayAdjustment.of(BusinessDayConventions.FOLLOWING, paymentCalId));
+				BusinessDayAdjustment.of(BusinessDayConventions.FOLLOWING,
+						HolidayCalendarId.defaultByCurrency(pair.getBase())));
 
 		return FxSingleTrade.builder().product(fx)
 				.info(TradeInfo.builder().id(StandardId.of("example", "2")).tradeDate(valueDate)
@@ -75,6 +87,23 @@ public class FxForwardTest {
 										pair.getCounter().getCode(), rate, tenor))
 						.counterparty(StandardId.of("example", "BigBankB")).settlementDate(paymentDate).build())
 				.build();
+	}
+
+	private static final FxNdfTrade createFxNdf(CurrencyPair pair, double amount, double rate, LocalDate valueDate,
+			Period tenor) {
+		LocalDate paymentDate = calcPaymentDate(pair, valueDate, tenor);
+
+		FxIndex index = ImmutableFxIndex.builder().name(pair.toString()).currencyPair(pair)
+				.fixingCalendar(HolidayCalendarIds.USNY).maturityDateOffset(
+						DaysAdjustment.ofBusinessDays(2, HolidayCalendarId.defaultByCurrency(pair.getBase())))
+				.build();
+
+		FxNdf product = FxNdf.builder().settlementCurrencyNotional(CurrencyAmount.of(USD, amount))
+				.agreedFxRate(FxRate.of(pair, rate)).index(index).paymentDate(paymentDate).build();
+
+		FxNdfTrade trade = FxNdfTrade.of(TradeInfo.empty(), product);
+
+		return trade;
 	}
 
 	public final Try<BuiltMarketData> getBuiltMarketData() {
@@ -127,6 +156,16 @@ public class FxForwardTest {
 				.onSuccess(v -> assertEquals(v.getAmount(), -53966.34033830941, EPSILON));
 		fwdRate.onFailure(t -> fail(t.getMessage()))
 				.onSuccess(r -> assertEquals(r.fxRate(r.getPair()), 1.298282658708429, EPSILON));
+	}
+
+	@Test
+	public void testFxNdf() {
+		Try<Results> results = price(List.of(USD_KRW_NDF_6M), List.of(Measures.PRESENT_VALUE));
+
+		Try<CurrencyAmount> npv = results.map(r -> r.get(0, 0)).map(r -> (CurrencyAmount) r.getValue());
+
+		npv.onFailure(t -> fail(t.getMessage()))
+				.onSuccess(v -> assertEquals(v.getAmount(), -53966.34033830941, EPSILON));
 	}
 
 	@Test
